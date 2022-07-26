c1b058934e0d03f9555e3a4ab01e54af4a68461c
Mo Xiuzhu <1799604429@qq.com>
2022-07-26 17:09:47 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
-// token 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
-const octokit = new Octokit({ auth: `personal-access-token123` });
+const github = require("@actions/github");
 
-octokit.rest.issues.create({
-  owner: "XiuzhuMo",
-  repo: "teach-create-issues-action",
-  title: "Hello world from " + slug,
-});
+// 获取octokit对象进行后续操作
+const octokit = github.getOctokit('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');
+
+// 执行函数
+(async () => {
+  try {
+    await octokit.rest.issues.create({
+      owner: "XiuzhuMo",
+      repo: "teach-create-issues-action",
+      title: "Hahahaaa ",
+      body: "Bhhkk ",
+    });
+  } catch (error) {
+    console.log("[ error ] >", error);
+  }
+})();
